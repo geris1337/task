60213dbb150a7c7d009a94bb45a496091e354875
geris1337 <50952907+geris1337@users.noreply.github.com>
2020-09-27 17:22:51 +0300

    Rewrite

--- test_task4.py
+++ test_task4.py
@@ -1,4 +1,4 @@
-import os, pytest
+import os, pytest, subprocess
 from os import listdir
 from os.path import isfile, join
 
@@ -8,8 +8,10 @@ def prepare():
 
     for item in files:
         if item.endswith("input.txt"):
-            os.system("python upper_case_file.py --input-file data/%s --output-file data/%s" % (item, item.replace("input.txt", "processed.txt")))
-            # Prepare comparison file multilist
+            inputfile = 'data/' + item
+            outputfile = 'data/' + item.replace("input.txt", "processed.txt")
+            subprocess.call(['python3', 'upper_case_file.py', '--input-file', inputfile, '--output-file', outputfile])
+	        # Prepare comparison file multilist
             first = "data/%s" % (item.replace("input.txt", "processed.txt")),
             second = "data/%s" % (item.replace("input.txt", "output.txt")),
             multilist += list(zip(first, second))
@@ -19,8 +21,8 @@ def prepare():
 @pytest.mark.parametrize("filepath", prepare())
 def test_min(filepath):
     with open(filepath[0], 'r') as file1:
-        data1 = file1.read().replace('\r\n', '\n')
+        data1 = file1.read().replace('\r\n', '\n').replace('\n', '')
     with open(filepath[1], 'r') as file2:
-        data2 = file2.read().replace('\r\n', '\n')
+        data2 = file2.read().replace('\r\n', '\n').replace('\n', '')
     
     assert data1 == data2
